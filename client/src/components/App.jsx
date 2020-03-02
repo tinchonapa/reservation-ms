f3962b06e54e0037adaef4df4bdc30c5d96b141c
tinchonapa <tinchonapa@gmail.com>
2020-03-01 23:38:13 -0700

    Created initial React structure

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import Form from './Form.jsx';
 
 class App extends React.Component {
     constructor(props){
         super(props);
-        this.state ={}
+        this.state ={
+            customers: []
+        }
+        this.addNewCustomer = this.addNewCustomer.bind(this)
+    }
+
+    componentDidMount() {
+        fetch('http:/localhost:3002/api/customers')
+            .then((res) => {
+                console.log('Initial res is ', res)
+                return res.json()
+            })
+            .then((data) => {
+                console.log('this is data ', data);
+                this.setState({
+                    customers: data.customers
+                })
+            })
+    }
+
+    addNewCustomer(newCustomerData) {
+        const id = this.state.customers.length + 1;
+        newCustomerData.id = id;
+
+        const newCustomer = this.state.customers.concat(newCustomerData);
+        this.setState({
+            customers: newCustomer
+        })
     }
 
 
@@ -11,6 +39,7 @@ class App extends React.Component {
         return (
          <div>
              <h1>Hello</h1>
+             <Form addNewCustomer={this.addNewCustomer}/>
          </div>
         )
     }
